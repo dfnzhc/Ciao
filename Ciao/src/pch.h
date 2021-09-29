eca2cef06a84484e0ec043c2941feda6bdb4064c
yutou <z69500jqk@qq.com>
2021-09-29 15:59:45 +0800

    清除 gitignore

--- Ciao/src/pch.h
+++ Ciao/src/pch.h
@@ -40,19 +40,3 @@
 #include <map>
 
 #include <windows.h>
-
-#include "Core/Common.h"
-#include "Core/Log.h"
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-#include <assimp/cimport.h>
-
-#include <meshoptimizer.h>
